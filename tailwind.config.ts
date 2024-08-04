7712f136eed2efda51022d930f5baf38aecdbfc8
sunnnyy04 <sunnyshukla4900@gmail.com>
2024-08-04 17:17:22 +0530

    signup done

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,15 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        customRed: '#e92742',
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+      }
+      ,
     },
   },
   plugins: [],
